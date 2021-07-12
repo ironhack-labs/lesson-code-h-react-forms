ea0d213c7693228de3e678a94eb954280741f5c5
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-12 21:36:48 +0200

    Example 1

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,14 +2,11 @@ import { useState } from "react";
 import moviesDataJSON from "../movies-data.json";
 import MovieCard from "./MovieCard";
 
-import AddMovie from "./AddMovie";
-
 function MovieList() {
   const [movies, setMovies] = useState(moviesDataJSON);
 
   return (
     <div>
-      <AddMovie />
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
