05ea2aa6e70233a82d117c5f2929d027acee2a03
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-12 21:35:36 +0200

    Example 2

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,37 +3,13 @@ import moviesDataJSON from "../movies-data.json";
 import MovieCard from "./MovieCard";
 
 import AddMovie from "./AddMovie";
-import FilterMovies from "./FilterMovies";
 
 function MovieList() {
-  const [moviesData, setMoviesData] = useState(moviesDataJSON);
   const [movies, setMovies] = useState(moviesDataJSON);
-
-  const addNewMovie = (movie) => {
-    const updatedMoviesData = [...moviesData, movie];
-    const updatedMovies = [...movies, movie];
-
-    setMoviesData(updatedMoviesData);
-    setMovies(updatedMovies);
-  };
-
-  const filterMovieList = (str) => {
-    let filteredMovies;
-    if (str === "All") {
-      filteredMovies = moviesData;
-    } else {
-      filteredMovies = moviesData.filter((movie) => {
-        return movie.title[0].toLowerCase() === str.toLowerCase();
-      });
-    }
-
-    setMovies(filteredMovies);
-  };
 
   return (
     <div>
-      <FilterMovies filterMovies={filterMovieList} />
-      <AddMovie addMovie={addNewMovie} />
+      <AddMovie />
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
