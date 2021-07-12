9aec0000cfebcd7b5ca12c00751d5f930665779e
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-12 21:38:10 +0200

    Example 2

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -6,30 +6,21 @@ function AddMovie(props) {
   const [IMDBRating, setIMDBRating] = useState(5);
   const [hasOscars, setHasOscars] = useState(true);
 
-  const handleTitleInput = (e) => setTitle(e.target.value);
-
-  const handleDirectorInput = (e) => setDirector(e.target.value);
-
-  const handleRatingInput = (e) => setIMDBRating(e.target.value);
-
-  // boolean value `checked` from the `checkbox` input
-  const handleOscarsInput = (e) => setHasOscars(e.target.checked);
-
   return (
     <div className="AddMovie">
       <h4>Add a Movie</h4>
       <form>
         <label>Title:</label>
-        <input type="text" name="title" value={title} onChange={handleTitleInput} />
+        <input type="text" name="title" value={title} />
 
         <label>Director:</label>
-        <input type="text" name="director" value={director} onChange={handleDirectorInput} />
+        <input type="text" name="director" value={director} />
 
         <label>IMDB Rating:</label>
-        <input type="number" name="IMDBRating" value={IMDBRating} onChange={handleRatingInput} />
+        <input type="number" name="IMDBRating" value={IMDBRating} />
 
         <label>Won Oscars:</label>
-        <input type="checkbox" name="hasOscars" checked={hasOscars} onChange={handleOscarsInput} />
+        <input type="checkbox" name="hasOscars" checked={hasOscars} />
         <button type="submit">Add a Movie</button>
       </form>
     </div>
